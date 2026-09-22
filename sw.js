58f9325c3ecd0a2e2a12c921da87c11e63492c9b
meh00-smkmk <mahboubiyounes00@gmail.com>
2026-09-22 20:42:02 +0100

    fix: invalidate stale guest cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sillage-cache-v1';
+const CACHE_NAME = 'sillage-cache-v2';
 const urlsToCache = [
   '/'
 ];
@@ -11,6 +11,8 @@ self.addEventListener('install', event => {
       })
   );
 });
+
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
